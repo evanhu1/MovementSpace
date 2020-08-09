389252032877a3c32639d0c947dc51c38b264b48
brandonshen <brandon69shen@gmail.com>
2020-08-09 00:13:58 -0700

    recipe page

--- Gemfile
+++ Gemfile
@@ -10,9 +10,6 @@ group :development, :test do
  gem 'sqlite3'
 end
 
-group :production do
-  gem 'pg'
-end
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-9.times do |i|
-    Recipe.create(
-      name: "Recipe #{i + 1}",
+
+Recipe.create(
+      name: "Black Lives Matter",     
       ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
       instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
-    )
-  end
+)
+Recipe.create(
+      name: "Me Too",     
+      ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
+      instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
+)
+Recipe.create(
+      name: "Green New Deal",
+      ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
+      instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
+)
+
+
