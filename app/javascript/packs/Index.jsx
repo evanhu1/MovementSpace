932d5bda7d3cd96e76c97dafac0142204dbfd1f8
evanhu1 <hu.evan123@gmail.com>
2020-08-09 00:13:12 -0700

    small changes

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Routes from "../routes/Index";
 import App from "../components/App";
 
-document.addEventListener("DOMContentLoaded", () => {
-  render(
-    <App />,
-    document.body.appendChild(document.createElement("div"))
-  );
-});
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
