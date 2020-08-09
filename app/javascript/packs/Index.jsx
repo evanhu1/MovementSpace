1de39b2b9d249c021fe9ecaaa387613beced9f66
evanhu1 <hu.evan123@gmail.com>
2020-08-09 03:54:07 -0700

    hotfix

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Routes from "../routes/Index";
+import Routes from "../routes/routes";
 import App from "../components/App";
 
 ReactDOM.render(
